7c656ed879ebe7c31f4841768194fb5cbe3b52bf
7422nj <pritamdas15217@gmail.com>
2021-03-05 22:11:18 -0500

    comments added in dowhileloop

--- src/Loops/DoWhileLoop.java
+++ src/Loops/DoWhileLoop.java
@@ -3,12 +3,16 @@ package Loops;
 public class DoWhileLoop {
     public static void main(String[] args) {
 
+        // Our Value
+        int i=1;
 
-        // Do While
-        int age=18;
+        // RUN At Least ONCE
         do{
-            System.out.println(age);
-            age--;
-        }while(age>18);
+            System.out.println(i);
+            i++;
 
-    }}
+            // If Value Meets Condition
+            // Will Run Continuously Until CCondition is Met
+        }while(i<=10); //Should Print 1,2,3,4,5,6,7,8,9,10
+    }
+}
