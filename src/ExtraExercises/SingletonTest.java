91f0a0b47f3b903f182d8b1b13224a7b0b774431
7422nj <pritamdas15217@gmail.com>
2021-03-09 13:04:22 -0500

    deleted unwanted classes

--- src/ExtraExercises/SingletonTest.java
+++
@@ -1,9 +0,0 @@
-package ExtraExercises;
-
-public class SingletonTest {
-   // private static Singleton single=new Singleton();
-
-    public static void main(String[] args) {
-        //Singleton obj= Singleton
-    }
-}
